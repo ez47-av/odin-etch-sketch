9f5ab278387e4ae06ad7e00e2b9f67d20ce6b936
ez47-av <257847801+ez47-av@users.noreply.github.com>
2026-03-02 11:29:24 -0700

    Create function to generate grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("test")
+const container = document.querySelector(".container");
+const div = document.createElement("div");
+const gridSize = 16;
+
+function createGrid(size) {
+  for (let i = 0; i < size*size; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+  }
+
+  const temp = document.querySelectorAll("div");
+  console.log(temp);
+}
+
+createGrid(gridSize);
